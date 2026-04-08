311a82a39f2e550384066e03199618094054088b
백재우 <9712jw@gmail.com>
2026-04-08 11:25:00 +0900

    Update build.gradle.kts

--- projects/spring-app/build.gradle.kts
+++ projects/spring-app/build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    // implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
